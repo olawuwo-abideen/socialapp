a815119f2a15c26fc0d71a556b7ef4481edb47b6
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-08-08 15:26:43 +0100

    fix

--- gateway/index.js
+++ gateway/index.js
@@ -9,11 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/user', proxy('http://localhost:5001'))
-app.use('/comment', proxy('http://localhost:5003'))
-app.use('/like', proxy('http://localhost:5002')) // product
-app.use('/post', proxy('http://localhost:5003'))
-app.use('/relationship', proxy('http://localhost:5003'))
-
+app.use('/comment', proxy('http://localhost:5002'))
+app.use('/like', proxy('http://localhost:5003')) 
+app.use('/post', proxy('http://localhost:5004'))
+app.use('/relationship', proxy('http://localhost:5004'))
+app.use('/relationship', proxy('http://localhost:5006'))
 
 app.listen(5000, () => {
     console.log('Gateway is Listening to Port 5000')
