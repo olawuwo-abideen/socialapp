8b5558b625f1bf1689d816133039bd412a28fb8c
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-08-28 20:57:21 +0100

    connect

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors')
+
+
+const app = express();
+
+
+
+
+
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome To my Api Chat api</h1>');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
